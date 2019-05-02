27d779bbfbc7b268904cc9aa8fdd8ea751b5e50d
Julien NOEL <julien.noel5@etu.univ-lorraine.fr>
2019-05-02 15:19:20 +0200

    Ajout des boutons radios et des combosBox dans la classe PanneauChoix

--- src/controleur/PanneauChoix.java
+++ src/controleur/PanneauChoix.java
@@ -12,6 +12,16 @@ public class PanneauChoix extends JPanel {
 
     public PanneauChoix(VueDessin vdessin){
         this.vdessin=vdessin;
+        JRadioButton newFig = new JRadioButton("Nouvelle figure");
+        JRadioButton mainLevee = new JRadioButton("Trace a main levee");
+        JRadioButton manip = new JRadioButton("Manipulations");
+        this.add(newFig);
+        this.add(mainLevee);
+        this.add(manip);
+        JComboBox formes = new JComboBox(new String[]{"Triangle", "Rectangle"});
+        JComboBox couleurs = new JComboBox(new String[] {"Noir","Rouge","Vert","Bleu","Jaune","Gris","Magenta","Rose"});
+        this.add(couleurs);
+        this.add(formes);
     }
 
     public Color determineCouleur(int couleur){
