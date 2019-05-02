6b7e63c0ac3e4bdd73c2acb793198d7e64cf978d
pa1007 <pa.fourriere@pa1007.fr>
2019-05-02 15:46:08 +0200

    Ajouts des listeners

--- src/controleur/PanneauChoix.java
+++ src/controleur/PanneauChoix.java
@@ -2,29 +2,68 @@ package controleur;
 
 import modele.DessinModele;
 import vue.VueDessin;
-
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PanneauChoix extends JPanel {
-    private VueDessin vdessin;
+
+    private VueDessin    vdessin;
     private DessinModele dmodele;
 
-    public PanneauChoix(VueDessin vdessin){
-        this.vdessin=vdessin;
-        JRadioButton newFig = new JRadioButton("Nouvelle figure");
-        JRadioButton mainLevee = new JRadioButton("Trace a main levee");
-        JRadioButton manip = new JRadioButton("Manipulations");
+    public PanneauChoix(VueDessin vdessin) {
+        this.vdessin = vdessin;
+        JRadioButton      newFig    = new JRadioButton("Nouvelle figure");
+        JRadioButton      mainLevee = new JRadioButton("Trace a main levee");
+        JRadioButton      manip     = new JRadioButton("Manipulations");
+        JComboBox<String> formes    = new JComboBox<>(new String[]{"Triangle", "Rectangle"});
+        JComboBox<String> couleurs = new JComboBox<>(new String[]{
+                "Noir",
+                "Rouge",
+                "Vert",
+                "Bleu",
+                "Jaune",
+                "Gris",
+                "Magenta",
+                "Rose"
+        });
+        formes.setEnabled(false);
+        ButtonGroup b = new ButtonGroup();
+        b.add(newFig);
+        b.add(mainLevee);
+        b.add(manip);
+        mainLevee.setSelected(true);
+        newFig.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formes.setEnabled(true);
+            }
+        });
+        mainLevee.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formes.setEnabled(false);
+                //todo a completer pour le dessin
+            }
+        });
+        manip.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
         this.add(newFig);
         this.add(mainLevee);
         this.add(manip);
-        JComboBox formes = new JComboBox(new String[]{"Triangle", "Rectangle"});
-        JComboBox couleurs = new JComboBox(new String[] {"Noir","Rouge","Vert","Bleu","Jaune","Gris","Magenta","Rose"});
-        this.add(couleurs);
         this.add(formes);
+        this.add(couleurs);
     }
 
-    public Color determineCouleur(int couleur){
+    public Color determineCouleur(int couleur) {
         return null;
     }
 }
