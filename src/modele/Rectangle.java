709fe993f0cff22bcce68539791a7e6bdffa0c9b
pa1007 <pa.fourriere@sfr.fr>
2019-05-04 22:38:08 +0200

    Creation des nouveaux type

---
+++ src/modele/Rectangle.java
@@ -0,0 +1,13 @@
+package modele;
+
+public class Rectangle extends Quadrilatere {
+
+    public Rectangle() {
+
+    }
+
+    @Override
+    public int nbPoints() {
+        return 2;
+    }
+}
---
+++ src/modele/Triangle.java
@@ -0,0 +1,9 @@
+package modele;
+
+public class Triangle extends Polygone {
+
+    @Override
+    public int nbPoints() {
+        return 3;
+    }
+}
