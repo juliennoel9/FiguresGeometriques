cb5c896303f8d55647da8bf137e975c73a73a836
Julien NOEL <juliennoel9@gmail.com>
2019-05-04 22:52:08 +0200

    Minor change (ajout du getter des couleurs)

--- src/modele/FigureColoree.java
+++ src/modele/FigureColoree.java
@@ -6,15 +6,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class FigureColoree {
-    private final static int TAILLE_CARRE_SELECTION=5;
-    protected Color couleur;
-    protected ArrayList<Point> tab_mem;
-    private boolean selected;
 
-    public FigureColoree(){
-           this.selected=false;
-           this.tab_mem=new ArrayList<>();
-           this.couleur=Color.BLACK;
+    private final static int         TAILLE_CARRE_SELECTION = 5;
+    protected            Color       couleur;
+    protected            List<Point> tab_mem;
+    private              boolean     selected;
+
+    public FigureColoree() {
+        this.selected = false;
+        this.tab_mem = new ArrayList<>();
+        this.couleur = Color.BLACK;
+    }
+
+    public Color getCouleur() {
+        return couleur;
     }
 
     public abstract int nbPoints();
@@ -23,19 +28,18 @@ public abstract class FigureColoree {
 
     public abstract void modifierPoints(List<Point> points);
 
-    public void affiche(Graphics g){
+    public abstract void affiche(Graphics g);
+
+
+    public void selectionne() {
 
     }
 
-    public void selectionne(){
+    public void deSelectionne() {
 
     }
 
-    public void deSelectionne(){
-
-    }
-
-    public void changeCouleur(Color color){
-
+    public void changeCouleur(Color color) {
+        couleur = color;
     }
 }
