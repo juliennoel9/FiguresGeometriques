3980f9f90b093a8aeac6dd80928456c766d12d65
Julien NOEL <juliennoel9@gmail.com>
2019-05-04 22:52:54 +0200

    Ajout du paintComponent

--- src/vue/VueDessin.java
+++ src/vue/VueDessin.java
@@ -1,7 +1,9 @@
 package vue;
 
 import modele.DessinModele;
+import modele.FigureColoree;
 import javax.swing.JPanel;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.Observable;
 import java.util.Observer;
@@ -11,15 +13,23 @@ public class VueDessin extends JPanel implements Observer {
     private DessinModele dessin;
 
     public VueDessin() {
-
+        setVisible(true);
+        setPreferredSize(new Dimension(100, 100));
     }
 
     @Override
     public void update(Observable o, Object arg) {
-
+        dessin = (DessinModele) o;
+        repaint();
     }
 
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        if (dessin != null) {
+            for (FigureColoree figureColoree : dessin.getListFigureColore()) {
+                g.setColor(figureColoree.getCouleur());
+                figureColoree.affiche(g);
+            }
+        }
     }
 }
