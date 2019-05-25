b8cf31c922db60ed2518651189379cc1b7d22d08
Julien NOEL <juliennoel9@gmail.com>
2019-05-26 00:20:57 +0200

    Refactor de la classe Trait

--- src/modele/Trait.java
+++ src/modele/Trait.java
@@ -8,34 +8,6 @@ import java.util.List;
 
 public class Trait extends FigureColoree {
 
-    /**
-     * Couleur du trait
-     */
-    private Color color;
-
-    /**
-     * Abscisse du point de depart du trait
-     */
-    private int pAx;
-
-    /**
-     * Ordonnee du point de depart du trait
-     */
-    private int pAy;
-
-    /**
-     * Abscisse du point d'arrive du trait
-     */
-    private int pBx;
-
-    /**
-     * Ordonnee du point d'arrive du trait
-     */
-    private int pBy;
-
-    /**
-     * Constructeur vide
-     */
     public Trait() {
 
     }
@@ -45,17 +17,13 @@ public class Trait extends FigureColoree {
      * ainsi que la couleur du trait
      *
      * @param couleur Couleur du trait
-     * @param pAx     Abscisse du point de depart du trait
-     * @param pAy     Ordonnee du point de depart du trait
-     * @param pBx     Abscisse du point d'arrive du trait
-     * @param pBy     Ordonnee du point d'arrive du trait
+     * @param p1     Point de depart du trait
+     * @param p2     Point d'arrive du trait
      */
-    public Trait(Color couleur, int pAx, int pAy, int pBx, int pBy) {
-        this.color = couleur;
-        this.pAx = pAx;
-        this.pAy = pAy;
-        this.pBx = pBx;
-        this.pBy = pBy;
+    public Trait(Color couleur, Point p1, Point p2) {
+        this.couleur=couleur;
+        tab_mem.add(p1);
+        tab_mem.add(p2);
     }
 
     /**
@@ -80,60 +48,73 @@ public class Trait extends FigureColoree {
 
     @Override
     public void modifierPoints(List<Point> points) {
-        setpAx(points.get(0).getX());
-        setpAy(points.get(0).getY());
-        setpBx(points.get(1).getX());
-        setpBy(points.get(1).getY());
+        tab_mem = points;
     }
 
     public void affiche(Graphics g) {
-        g.setColor(this.color);
-        g.drawLine(pAx, pAy, pBx, pBy);
+        g.setColor(this.couleur);
+        g.drawLine(tab_mem.get(0).getX(), tab_mem.get(0).getY(), tab_mem.get(1).getX(), tab_mem.get(1).getY());
+        super.affiche(g);
     }
 
     @Override
     public boolean isInSelection(MouseEvent e) {
+        int last_x = e.getX();
+        int last_y = e.getY();
+
+        if (!tab_mem.isEmpty()){
+            Point              p1       = tab_mem.get(0);
+            Point              p2       = tab_mem.get(1);
+
+            int distX;
+            int distY;
+            int xRect;
+            int yRect;
+
+            if (p1.getX()<p2.getX()){
+                xRect=p1.getX();
+                distX = Math.abs(p2.getX()-p1.getX());
+            }else if (p1.getX()>p2.getX()){
+                xRect=p2.getX();
+                distX = Math.abs(p2.getX()-p1.getX());
+            }else {
+                xRect=p1.getX()-5;
+                distX = Math.abs(p2.getX()-p1.getX())+5;
+            }
+
+            if (p1.getY()<p2.getY()){
+                yRect=p1.getY();
+                distY = Math.abs(p2.getY()-p1.getY());
+            }else if (p1.getY()>p2.getY()){
+                yRect=p2.getY();
+                distY = Math.abs(p2.getY()-p1.getY());
+            }else {
+                yRect=p1.getY()-5;
+                distY = Math.abs(p2.getY()-p1.getY())+5;
+            }
+
+            int distance = (int) Math.abs(p1.distance(p2));
+
+            if (distance>5){
+                java.awt.Rectangle r        = new java.awt.Rectangle(xRect, yRect, distX, distY);
+                if (r.contains(last_x, last_y)) {
+                    selectionne();
+                    return true;
+                }
+                return false;
+            }else {
+                if (Math.pow(p1.getX()-last_x,2)+Math.pow(p1.getY()-last_y,2)<=400){
+                    selectionne();
+                    return true;
+                }
+                return false;
+            }
+        }
         return false;
     }
 
     @Override
     public FabricantFigure getContructeur(DessinModele dessinModele) {
         return new FabricantFigure(this, dessinModele);
-    }
-
-    /**
-     * Setter de l'abscisse du point de depart du trait
-     *
-     * @param pAx Abscisse du point de depart du trait
-     */
-    public void setpAx(int pAx) {
-        this.pAx = pAx;
-    }
-
-    /**
-     * Setter de l'ordonnee du point de depart du trait
-     *
-     * @param pAy Ordonnee du point de depart du trait
-     */
-    public void setpAy(int pAy) {
-        this.pAy = pAy;
-    }
-
-    /**
-     * Setter de l'abscisse du point d'arrive du trait
-     *
-     * @param pBx Abscisse du point d'arrive du trait
-     */
-    public void setpBx(int pBx) {
-        this.pBx = pBx;
-    }
-
-    /**
-     * Setter de l'ordonnee du point d'arrive du trait
-     *
-     * @param pBy Ordonnee du point d'arrive du trait
-     */
-    public void setpBy(int pBy) {
-        this.pBy = pBy;
     }
 }
